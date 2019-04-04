8a0d3c687ab636ca302342f2e56f5c7a2abe51a0
mkubibaev <mkubibaev@gmail.com>
2019-04-04 15:33:35 +0600

    get link by shortLink and save new link

--- app/links.js
+++ app/links.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const path = require('path');
 const nanoid = require('nanoid');
+const Link = require('../models/Link');
 
 const router = express.Router();
 
 router.get('/:shortUrl', (req, res) => {
-
+    Link.findOne(req.params.shortUrl)
+        .then(result => {
+            if (result) {
+                return res.status(301).redirect(result.originalUrl);
+            }
+            res.sendStatus(404);
+        })
+        .catch(() => res.sendStatus(500));
 });
 
 router.post('/', (req, res) => {
+    const newLink = req.body;
+
+    newLink.shortUrl = nanoid(7);
+
+    const link = new Link(newLink);
+    link.save()
+        .then(result => res.send(result))
+        .catch(() => res.status(400).send(error));
 
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const links = require('./app/links');
 
 const app = express();
 const port = 8000;
@@ -9,9 +10,9 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
 
-mongoose.connect('mongodb://localhost/shop', {useNewUrlParser: true}).then(() => {
+mongoose.connect('mongodb://localhost/link_shortener', {useNewUrlParser: true}).then(() => {
 
-    app.use('/products', products);
+    app.use('/links', links);
 
     app.listen(port, () => {
         console.log(`Server started on ${port} port`);
