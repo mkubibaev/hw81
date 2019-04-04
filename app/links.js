d885f324a41993456a695d4c7b4aa0a01753af4c
mkubibaev <mkubibaev@gmail.com>
2019-04-04 14:29:33 +0600

    added links route

---
+++ app/links.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const nanoid = require('nanoid');
+
+const router = express.Router();
+
+router.get('/:shortUrl', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/shop', {useNewUrlParser: true}).then(() => {
+
+    app.use('/products', products);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`);
+    });
+});
