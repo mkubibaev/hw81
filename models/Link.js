9ab06e891336424fff0d860c5dcd47951e71940a
mkubibaev <mkubibaev@gmail.com>
2019-04-04 14:32:51 +0600

    added Link model

---
+++ models/Link.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const LinkSchema = new Schema({
+    shortUrl: {
+        type: String,
+        required: true
+    },
+    originalUrl : {
+        type: String,
+        required: true
+    },
+});
+
+const Link = mongoose.model('Link', LinkSchema);
+
+module.exports = Link;
